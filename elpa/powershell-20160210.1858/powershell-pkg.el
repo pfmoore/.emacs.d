a3cba7081ce3cdca8c8b0ea3a3e6572ff880d908
Paul Moore <p.f.moore@gmail.com>
2016-05-10 18:40:18 +0100

    Initial commit

---
+++ elpa/powershell-20160210.1858/powershell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "powershell" "20160210.1858" "Mode for editing Powershell scripts" '((emacs "24")) :url "http://github.com/jschaf/powershell.el" :keywords '("powershell" "languages"))
---
+++ init.el
@@ -0,0 +1,107 @@
+(setq url-proxy-services '(("http" . "localhost:3128")
+			   ("https" . "localhost:3128")
+			   ))
+
+;; Setup package control
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+(require 'use-package)
+
+;; Specifies local directory to load packages from
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(let ((default-directory  "~/.emacs.d/packages/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(condition-case nil
+    (set-face-attribute 'default nil :font "Deja Vu Sans Mono-12")
+  (error
+   (condition-case nil
+       (set-face-attribute 'default nil :font "Consolas-12")
+     (error
+      (condition-case nil
+	  (set-face-attribute 'default nil :font "Courier New-12")
+	(error nil))))))
+
+;(require use-package)
+
+;; Essential settings.
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(tool-bar-mode -1) ; No toolbar
+(scroll-bar-mode -1) ; Hide scrollbars
+(menu-bar-mode -1) ; Hide menu bar
+(show-paren-mode t) ; Highlights matching parenthesis
+(electric-pair-mode t)
+(setq initial-scratch-message "") ; No scratch text
+(setq-default show-trailing-whitespace t) ; Shows all trailing whitespace
+
+(use-package sublime-themes
+  :ensure t
+  :config
+  (load-theme 'spolsky t)) ; Color theme
+
+;; Base evil package
+(use-package evil
+  :ensure t
+  :init
+  ;; Unbind <C-u> for evil mode's use
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode t)
+  ;; Move up and down through wrapped lines
+  (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+  (setq evil-split-window-below t)
+  (setq evil-vsplit-window-right t))
+
+;; evil leader key
+(use-package evil-leader
+  :ensure t
+  :config
+  (evil-leader/set-leader "<SPC>")
+  (setq evil-leader/in-all-states 1)
+  (global-evil-leader-mode)
+  (evil-leader/set-key
+    "w"  'save-buffer ; w(rite)
+    "so" 'eval-buffer ; so(urce)
+    "S" 'eval-defun ; S(ource)
+    "bb" 'mode-line-other-buffer ; b(ack) b(buffer)
+    "bn" 'next-buffer ; b(uffer) n(ext)
+    "bp" 'previous-buffer ; b(uffer) p(revious)
+    "bd" 'kill-buffer ; b(uffer) d(elete)
+    "bl" 'helm-buffers-list ; b(uffer) l(ist)
+    "init" (lambda() (interactive) (evil-buffer-new nil "~/.emacs.d/init.el"))))
+
+;; Tpope's surround
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
+;; Narrowing completion engine
+(use-package helm
+  :ensure t
+  :config
+  (helm-autoresize-mode 1)
+  (global-set-key (kbd "M-x")     'undefined)
+  (global-set-key (kbd "M-x")     'helm-M-x)
+  (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-x C-b") 'undefined)
+  (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+  (helm-mode t))
+
+(use-package linum-relative
+  :ensure t
+  :config
+  (setq linum-relative-current-symbol "")
+  (global-linum-mode t)
+  (linum-relative-mode))
